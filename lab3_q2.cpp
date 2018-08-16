d64510486de8d9c6004716bc96a55ea1936d452d
Soumik77777 <42136372+Soumik77777@users.noreply.github.com>
2018-08-16 20:34:46 +0530

    Add files via upload

---
+++ lab3_q2.cpp
@@ -0,0 +1,23 @@
+// First include the library
+#include<iostream>
+using namespace std;
+//maths codes begin
+int main ()
+//process
+{
+int a,b,addition,substraction,multiplication,division,modulus;
+   a=11;
+   b=5;
+addition=a+b; //adding integers;
+substraction=a-b; //substracting integers;
+multiplication=a*b; //multiply integers;
+division=a/b; //devide integers;
+modulus=a%b; //showing remainder;
+
+cout<<addition<<endl;
+cout<<substraction<<endl;
+cout<<multiplication<<endl;
+cout<<division<<endl;
+cout<<modulus<<endl;
+return 0;
+}
